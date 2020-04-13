b79ad282c1ee07e7e26ca776e056fe2aea64fffc
beloglazova <minam.dancer@yandex.ru>
2020-04-13 19:30:39 +0700

    Version

---
+++ Models/Version.cs
@@ -0,0 +1,9 @@
+namespace project.Models
+{
+    public class Version
+    {
+        public string Company { get; set; }
+        public string ProductVersion { get; set; }
+        public string Product { get; set; }
+    }
+}
