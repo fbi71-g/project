f8d71006dfd0ff98126934946d3420e28db11f2c
beloglazova <minam.dancer@yandex.ru>
2020-04-13 20:52:18 +0700

    Controller

--- Controllers/Lab1Controller.cs
+++ Controllers/Lab1Controller.cs
@@ -1,56 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
 using project.Models;
 using project.Storage;
 
 namespace project.Controllers
 {
-   [Route("api/[controller]")]
-   [ApiController]
-   public class LabController : ControllerBase
-   {
-      private static IStorage<Lab1Data> _memCache = new MemCache();
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LabController : ControllerBase
+    {
+        private static IStorage<Lab1Data> _memCache = new MemCache();
 
-      [HttpGet]
-       public ActionResult<IEnumerable<Lab1Data>> Get()
-       {
-           return Ok(_memCache);
-       }
-       [HttpGet("{id}")]
-       public ActionResult<Lab1Data> Get(int id)
-      {
-           if (_memCache.Count <= id) return NotFound("No such");
-          return Ok(_memCache[id]);
-       }
-     [HttpPost]
-      public IActionResult Post([FromBody] Lab1Data value)
-       {
-           var validationResult = value.Validate();
-          if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
-           _memCache.Add(value);
-           return Ok($"{value.ToString()} has been added");
-       }
-       [HttpPut("{id}")]
-      public IActionResult Put(int id, [FromBody] Lab1Data value)
-       {
-           if (_memCache.Count <= id) return NotFound("No such");
-           var validationResult = value.Validate();
-          if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
-           var previousValue = _memCache[id];
-           _memCache[id] = value;
-           return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
-       }
+        [HttpGet]
+        public ActionResult<IEnumerable<Lab1Data>> Get()
+        {
+            return Ok(_memCache.All);
+        }
 
+        [HttpGet("{id}")]
+        public ActionResult<Lab1Data> Get(Guid id)
+        {
+            if (!_memCache.Has(id)) return NotFound("No such");
 
-       [HttpDelete("{id}")]
+            return Ok(_memCache[id]);
+        }
 
-       public IActionResult Delete(int id)
-       {
-           if (_memCache.Count <= id) return NotFound("No such");
-           var valueToRemove = _memCache[id];
-           _memCache.RemoveAt(id);
-           return Ok($"{valueToRemove.ToString()} has been removed");
-       }
-}
+        [HttpPost]
+        public IActionResult Post([FromBody] Lab1Data value)
+        {
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
+            _memCache.Add(value);
+
+            return Ok($"{value.ToString()} has been added");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Lab1Data value)
+        {
+            if (!_memCache.Has(id)) return NotFound("No such");
+
+            var validationResult = value.Validate();
+
+            if (!validationResult.IsValid) return BadRequest(validationResult.Errors);
+
+            var previousValue = _memCache[id];
+            _memCache[id] = value;
+
+            return Ok($"{previousValue.ToString()} has been updated to {value.ToString()}");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            if (!_memCache.Has(id)) return NotFound("No such");
+
+            var valueToRemove = _memCache[id];
+            _memCache.RemoveAt(id);
+
+            return Ok($"{valueToRemove.ToString()} has been removed");
+        }
+    }
 }
